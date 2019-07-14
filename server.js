e537dcf9678259a5e07f4aa1dc76f831741ecef9
Jean Francois Itangayenda <janmucyo@gmail.com>
2019-07-14 16:05:53 +0900

    changed server.js, added methods get and use and sendFile

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const fs = require("fs");
-app.use(express.static(__dirname + "/public"));
+//app.use(express.static(__dirname + "/public"));
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static(__dirname + "/public"));
+}
+app.get('*', (req, res) =>{
+  res.sendFile(path.join(__dirname, "/public", "index.html"));
+});
 app.listen(process.env.PORT || 3000);
 module.exports = {app}
